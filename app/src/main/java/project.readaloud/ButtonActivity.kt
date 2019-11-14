d771a75ff1bbe653a8fa26999707027ed66aabbc
Abigail Duff <abigailduff98@gmail.com>
2019-11-14 17:42:37 -0500

    fixing?

---
+++ app/src/main/java/project.readaloud/ButtonActivity.kt
@@ -0,0 +1,38 @@
+package project.readaloud
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+
+class ButtonActivity : Activity() {
+
+    companion object {
+        // For stuff if needed
+    }
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+
+    // Set an OnClickListener on readButton
+    // Called each time the user clicks the Button
+    fun clickRead(v: View) {
+
+        val goToListIntent = Intent(
+                this@ButtonActivity,
+                ListViewActivity::class.java
+        )
+        startActivity(goToListIntent)
+
+    }
+
+    // Set an OnClickListener on createButton
+    // Called each time the user clicks the Button
+    fun clickCreate(v: View) {
+
+        // TODO: implement story creation
+
+    }
+}
