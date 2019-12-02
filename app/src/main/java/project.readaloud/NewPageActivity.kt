2f62130107298539fc82596dd0fa2a0e606ad70b
Matthew Goulding <goulding@terpmail.umd.edu>
2019-12-02 12:46:10 -0500

    added onclicks for the prev and next buttons

--- app/src/main/java/project.readaloud/NewPageActivity.kt
+++ app/src/main/java/project.readaloud/NewPageActivity.kt
@@ -1,11 +1,34 @@
 package project.readaloud
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 
 class NewPageActivity : Activity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.new_page)
+    }
+
+    fun prevPage(v: View) {
+
+        val int = Intent(
+                this@NewPageActivity,
+                NewPageActivity::class.java
+        )
+        startActivity(int)
+
+    }
+
+    // Set an OnClickListener on createButton
+    // Called each time the user clicks the Button
+    fun nextPage(v: View) {
+        val intent = Intent(
+                this@NewPageActivity,
+                NewPageActivity::class.java
+        )
+        startActivity(intent)
+
     }
 }
