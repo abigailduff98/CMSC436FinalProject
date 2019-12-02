5d798c674d4ea1fd941847ec82e1c856b6708789
Abigail Duff <abigailduff98@gmail.com>
2019-12-02 17:10:04 -0500

    adjusted appearance of reader page

--- app/src/main/java/project.readaloud/ReaderActivity.kt
+++ app/src/main/java/project.readaloud/ReaderActivity.kt
@@ -1,2 +1,15 @@
 package project.readaloud
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+
+class ReaderActivity : Activity() {
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.reader_page)
+    }
+
+}
