6ca4fe0efdc7dbd611a3277ca91dfc7ff166585b
Matthew Goulding <goulding@terpmail.umd.edu>
2019-12-02 21:27:05 -0500

    added access to book title text

--- app/src/main/java/project.readaloud/NewBookActivity.kt
+++ app/src/main/java/project.readaloud/NewBookActivity.kt
@@ -4,10 +4,15 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 
 class NewBookActivity : Activity(){
+    var bookTitle : EditText? = null
+
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        bookTitle = findViewById(R.id.bookTitleEditText)
         setContentView(R.layout.new_book)
     }
 
