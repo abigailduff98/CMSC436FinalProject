9a93c5bcf5dbb249f91c97d9effecf67801b39e8
stephg393 <stephanie.gnammakou@gmail.com>
2019-12-03 00:07:46 -0500

    pages

--- app/src/main/java/project.readaloud/NewPageActivity.kt
+++ app/src/main/java/project.readaloud/NewPageActivity.kt
@@ -4,13 +4,27 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import project.readaloud.R.layout.new_page
 
 class NewPageActivity : Activity() {
+    var text : EditText? = null
+    private lateinit var saveButton : Button
+    private lateinit var nextButton : Button
+
 
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.new_page)
+        text = findViewById(R.id.storyText)
+        saveButton = findViewById(R.id.saveButton)
+        nextButton = findViewById(R.id.nextPageButton)
+
+        saveButton.setOnClickListener(){
+
+        }
+        setContentView(new_page)
     }
 
     //save previous content and be able to show it
@@ -19,17 +33,13 @@ class NewPageActivity : Activity() {
     //save instance state
     //keep a stack of activities
     //save button (finish)
-    fun prevPage(v: View) {
 
-        val intent = Intent(
-                this@NewPageActivity,
-                NewPageActivity::class.java
-        )
-        startActivity(intent)
+    //save entire book to database
 
-    }
 
-    fun savePage(v: View) {
+
+
+    fun save() {
         val intent = Intent(
                 this@NewPageActivity,
                 CreateBookActivity::class.java
@@ -39,7 +49,8 @@ class NewPageActivity : Activity() {
 
     // Set an OnClickListener on createButton
     // Called each time the user clicks the Button
-    fun nextPage(v: View) {
+    //save to external
+    fun nextPage() {
         val intent = Intent(
                 this@NewPageActivity,
                 NewPageActivity::class.java
