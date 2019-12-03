2f3f20aef80e9b6fb6105eb131690a12106ba934
stephg393 <stephanie.gnammakou@gmail.com>
2019-12-02 20:43:08 -0500

    created objects

--- app/src/main/java/project.readaloud/Objects/Book.kt
+++ app/src/main/java/project.readaloud/Objects/Book.kt
@@ -1,5 +1,22 @@
 package project.readaloud.Objects
 
 class Book {
+    var title: String? = null
+    var pagesInBook: ArrayList<Page>? = null
+
+    constructor(title:String){
+        this.title = title
+
+        this.pagesInBook = ArrayList<Page>()
+    }
+
+
+    fun addPage(pageToAdd:Page){
+        pagesInBook?.add(pageToAdd)
+    }
+
+    fun deletePage(pageToDel:Page){
+        pagesInBook?.remove(pageToDel)
+    }
 
 }
--- app/src/main/java/project.readaloud/Objects/Page.kt
+++ app/src/main/java/project.readaloud/Objects/Page.kt
@@ -1,4 +1,17 @@
 package project.readaloud.Objects
 
+import android.media.Image
+
 class Page {
+    var text: String? = null
+    var image: Image? = null
+
+    constructor(text:String, image:Image){
+        this.text = text
+        this.image = image
+
+    }
+
+
+
 }
